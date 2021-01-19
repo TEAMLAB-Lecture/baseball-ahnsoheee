908060f295f140f8291644e13619e3349d211ada
ahnsoheee <soheeeahn@gmail.com>
2021-01-19 18:42:05 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -284,28 +284,36 @@ def main():
     print("Random Number is : ", random_number)
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
-    while(1):
-        print('Input guess number : ', end='')
-        user_input = input()
+    isEnd = False
+    while(isEnd == False):
+        user_input = input('Input guess number : ')
         if user_input == '0':
+            isEnd = True
             break
-        if is_validated_number(user_input) == False:
-            print('Wrong Input, Input again')
-        else:
+        elif is_validated_number(user_input):
             result = get_strikes_or_ball(user_input, random_number)
             print('Strikes : ', result[0], ' , Balls : ', result[1])
-
+        
             if result == [3, 0]:
-                print('You win, one more(Y/N) ?', end='')
-                retry = input()
-                while(retry == is_yes(retry) or retry == is_no(retry)):
-                    retry = input()
-                
-                if is_yes(retry):
-                    random_number = str(get_not_duplicated_three_digit_number())
-                    print("Random Number is : ", random_number)
-                elif is_no(retry):
-                    break
+                while(isEnd == False):
+                    retry = input('You win, one more(Y/N) ?')
+
+                    if is_yes(retry):
+                        random_number = str(get_not_duplicated_three_digit_number())
+                        print("Random Number is : ", random_number)
+                        break
+                    
+                    elif is_no(retry):
+                        isEnd = True
+                        break
+                    
+                    else:
+                        print('Wrong Input, Input again')
+        else:
+            print('Wrong Input, Input again')
+
+        
+
     # ==================================
     print('Thank you for using this program')
     print('End of the Game')
